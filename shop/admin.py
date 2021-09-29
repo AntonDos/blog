233b230d1c57d656987a763a2cef590d2523adc9
AntonDos <adosmenesk@pm.me>
2021-09-29 19:41:40 +0300

    new4

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from shop.models import Product, Purchase
+
+
+class PurchaseInline(admin.TabularInline):
+    model = Purchase
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "status")
+    search_fields = ("name",)
+    inlines = [
+        PurchaseInline,
+    ]
+
+
+@admin.register(Purchase)
+class PurchaseAdmin(admin.ModelAdmin):
+    list_display = ("user", "product", "count")
+    search_fields = ("user__email", "product__name")
---
+++ shop/spiders.py
@@ -0,0 +1,21 @@
+import logging
+
+import scrapy
+
+logger = logging.getLogger(__name__)
+
+
+class OmaSpider(scrapy.Spider):
+    name = "oma.by"
+    allowed_domains = ["https://www.oma.by"]
+    start_urls = ["https://www.oma.by/elektroinstrument-c"]
+
+    def parse(self, response, **kwargs):
+        for product in response.css(".catalog-grid .product-item"):
+            data = {
+                "title": product.css(".product-title-and-rate-block .wrapper::text").get().strip(),
+                "price": product.css(".product-price-block .price__normal::text").get().strip(),
+                "link": f"{self.allowed_domains[0]}{product.css('a.area-link::attr(href)').get()}",
+            }
+            logger.debug(data)
+            yield data
