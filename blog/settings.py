d6ab92941163403040b08eae8d3e54e29a8a4d2e
AntonDos <adosmenesk@pm.me>
2021-10-13 20:56:33 +0300

    6

--- blog/settings.py
+++ blog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'posts',
     'shop',
+    'django-rq',
 ]
 
 MIDDLEWARE = [
@@ -167,4 +168,14 @@ LOGGING = {
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+}
+
+
+RQ_QUEUES = {
+   "default": {
+       "HOST": "localhost",
+       "PORT": 6379,
+       "DB": 0,
+       "DEFAULT_TIMEOUT": 360,
+   },
 }
